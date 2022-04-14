7ff7b7d47d77152fdd279b2d2e4e5f6baa5fb851
dd <diegofernandez@motumweb.com>
2022-04-14 12:21:53 -0500

    refactor indexed_db structure

--- indexed_db/simplest/config.js
+++
@@ -1,5 +0,0 @@
-export  const DB_NAME = "db_name";
-export const DB_VERSION = 1;
-export  const OBJECT_STORE_NAME = "object_store_name";
-export  const KEY_PATH = "id";
-export  const KEY_AUTO_INC = false;
---
+++ indexed_db/simplest_modular/idb/config.js
@@ -0,0 +1,8 @@
+export const DB_NAME = "db_name";
+export const DB_VERSION = 1;
+export const OBJECT_STORE_NAME = "object_store_name";
+export const KEY_PATH = "id";
+export const KEY_AUTO_INC = false;
+export const OBJECT_STORES = 1;
+//  {"name": OBJECT_STORE_NAME, "option": {"keyPath": KEY_PATH}};
+//export const MAX_RETRY_MILLIS = 3000;
